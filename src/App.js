33bf78eb1e32750fe9e38dcb4b8f8fc169044771
k2hyun4 <k2hyun4@naver.com>
2020-11-03 17:38:13 +0900

    life-cycle functions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import PropTypes from "prop-types"
 
 class App extends React.Component {
+  //Mounting function
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
   state = {
     count: 0
   };
@@ -14,8 +20,25 @@ class App extends React.Component {
     this.setState(current => ({count: current.count - 1}));
   };
 
+  //Mounting function
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  //Updating function
+  componentDidUpdate() {
+    console.log("component updated");
+  }
+
+  //Unmounting function
+  componentWillUnmount() {
+    console.log("will unmount");
+  }
+
+  //Mounting function
   //react doesn't refresh "render" function
   render() {
+    console.log("render");
     return <div>
       <h1>The number is {this.state.count}</h1>  
       <button onClick={this.add}>Add</button>

a8f7a3f33d954d53f484ae321c693bd630bec4ae
k2hyun4 <k2hyun4@naver.com>
2020-11-05 11:46:39 +0900

    데이터 받아와서 뿌려주기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import axios from "axios";
+import Movie from "./Movie"
 
 class App extends React.Component {
   state = {
-    isLoading: true, 
+    isLoading: true,
     movies: []
   };
 
   //async, await, axios 와우
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    //movies.data.data.movies를 이렇게 축약 가능(es6)
+    //변수명은 그냥 json에 선언된걸 따라가는 듯
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    //{movies: movies}라고 해야하지만,, 알아서 잘 처리해줌
+    this.setState({ movies, isLoading: false });
   }
 
   componentDidMount() {
@@ -17,9 +26,17 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return <div>
-      {isLoading ? "Loading" : "We are ready"}
+      {isLoading ? "Loading,,," : movies.map(movie => (
+        <Movie 
+          key={movie.id}
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image}/>
+        ))}
     </div>
   }
 }
